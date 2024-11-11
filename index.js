063210b4866de13c228e652711facc9e7f3c7b47
Chieh-Yu <ccychen915@gmail.com>
2024-11-11 13:03:07 -0500

    3.2 complete

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
-//const express = require('express')
-const app = express()
+
+const app = express();
+
 Hello(app)
 Lab5(app);
-app.listen(process.env.PORT ||4000)
+app.listen(process.env.PORT || 4000)
