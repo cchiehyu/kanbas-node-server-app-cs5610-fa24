acf24c42cdcb821daf16a360ac0b4fb4ef17a1d4
Chieh-Yu <ccychen915@gmail.com>
2024-11-11 19:34:17 -0500

    3.6.3

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -76,4 +76,16 @@ export default function WorkingWithArrays(app) {
     res.sendStatus(200);
   });
 
+  app.put("/lab5/todos/:id", (req, res) => {
+    const { id } = req.params;
+    todos = todos.map((t) => {
+      if (t.id === parseInt(id)) {
+        return { ...t, ...req.body };
+      }
+      return t;
+    });
+    res.sendStatus(200);
+  });
+
+
 };
