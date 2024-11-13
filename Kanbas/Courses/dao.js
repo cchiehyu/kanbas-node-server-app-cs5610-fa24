c556b7d22936c7856190429f4c00095844e89530
Chieh-Yu <ccychen915@gmail.com>
2024-11-12 19:59:15 -0500

    4.4.3

--- Kanbas/Courses/dao.js
+++ Kanbas/Courses/dao.js
@@ -1,4 +1,12 @@
 import Database from "../Database/index.js";
+
 export function findAllCourses() {
   return Database.courses;
 }
+
+export function createCourse(course) {
+    const newCourse = { ...course, _id: Date.now().toString() };
+    Database.courses = [...Database.courses, newCourse];
+    return newCourse;
+  }
+  
