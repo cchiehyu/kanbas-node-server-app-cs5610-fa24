b20238d77d0ee844b594ae01c413ebb214b6a75e
Chieh-Yu <ccychen915@gmail.com>
2024-11-13 19:53:04 -0500

    finished 4.4.3

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,7 +1,15 @@
 import * as dao from "./dao.js";
+
 export default function CourseRoutes(app) {
   app.get("/api/courses", (req, res) => {
     const courses = dao.findAllCourses();
     res.send(courses);
+  });
+
+  // Move the delete route inside the CourseRoutes function
+  app.delete("/api/courses/:courseId", (req, res) => {
+    const { courseId } = req.params;
+    dao.deleteCourse(courseId);
+    res.sendStatus(204);
   });
 }
