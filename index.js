f36a294a24782b0d7e8577a9b94bb11a76ff5869
Chieh-Yu <ccychen915@gmail.com>
2024-11-24 19:28:43 -0500

    finished 3.2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+import "dotenv/config";
 import express from 'express';
+import mongoose from 'mongoose';
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
@@ -9,6 +11,10 @@ import session from "express-session";
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
 import AssignmentRoutes from './Kanbas/Courses/Assignments/routes.js';
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 app.use(cors({
     credentials: true,
