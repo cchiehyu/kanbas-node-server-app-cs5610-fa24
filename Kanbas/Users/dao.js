0a7786501ea5eef32390e68e454b184712d71c52
Chieh-Yu <ccychen915@gmail.com>
2024-11-28 16:31:15 -0500

    3.6.3 finish

--- Kanbas/Users/dao.js
+++ Kanbas/Users/dao.js
@@ -13,3 +13,11 @@ export const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $
 export const deleteUser = (userId) => model.deleteOne({ _id: userId });
 
 
+export const findUsersByRole = (role) => model.find({ role: role }); // or just model.find({ role })
+
+export const findUsersByPartialName = (partialName) => {
+  const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+  return model.find({
+    $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+  });
+};
--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -16,8 +16,21 @@ export default function UserRoutes(app) {
   const deleteUser = (req, res) => { };
 
   const findAllUsers = async (req, res) => {
+    const { role, name } = req.query;
+    if (role) {
+      const users = await dao.findUsersByRole(role);
+      res.json(users);
+      return;
+    }
+    if (name) {
+      const users = await dao.findUsersByPartialName(name);
+      res.json(users);
+      return;
+    }
+
     const users = await dao.findAllUsers();
     res.json(users);
+
   };
 
   const findUserById = (req, res) => { };
