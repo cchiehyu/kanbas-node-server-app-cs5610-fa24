fbb6980a4a7a5d00fd74bc613f08262c95995857
Chieh-Yu <ccychen915@gmail.com>
2024-11-29 18:19:43 -0500

    5.1.4

--- Kanbas/Courses/dao.js
+++ Kanbas/Courses/dao.js
@@ -22,18 +22,29 @@ export function createCourse(course) {
   //return newCourse;
  }
 
-  export function deleteCourse(courseId) {
-    const { courses, enrollments } = Database;
-    Database.courses = courses.filter((course) => course._id !== courseId);
-    Database.enrollments = enrollments.filter(
-      (enrollment) => enrollment.course !== courseId
-  );}
+ export async function deleteCourse(courseId) {
+  try {
+    // Delete the course document
+    const result = await model.deleteOne({ _id: courseId });
+    
+    // If course was successfully deleted, clean up related enrollments
+    if (result.deletedCount > 0) {
+      // Assuming you have an enrollment model
+      await enrollmentModel.deleteMany({ course: courseId });
+    }
+    
+    return result;
+  } catch (error) {
+    throw new Error(`Failed to delete course: ${error.message}`);
+  }
+}
   
   export function updateCourse(courseId, courseUpdates) {
-    const { courses } = Database;
-    const course = courses.find((course) => course._id === courseId);
-    Object.assign(course, courseUpdates);
-    return course;
+    return model.updateOne({ _id: courseId }, { $set: courseUpdates });
+    //const { courses } = Database;
+    //const course = courses.find((course) => course._id === courseId);
+    //Object.assign(course, courseUpdates);
+    //return course;
   }
   
   export function findCoursesForEnrolledUser(userId) {
