a38096998d577f186be8edc8cf2be9a7c1046977
Chieh-Yu <ccychen915@gmail.com>
2024-11-29 19:38:42 -0500

    5.3.1

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -37,3 +37,19 @@ export const createEnrollment = (enrollment) => {
   enrollments.push(newEnrollment);
   return newEnrollment;
 };
+
+export async function findCoursesForUser(userId) {
+  const enrollments = await model.find({ user: userId }).populate("course");
+  return enrollments.map((enrollment) => enrollment.course);
+ }
+ export async function findUsersForCourse(courseId) {
+  const enrollments = await model.find({ course: courseId }).populate("user");
+  return enrollments.map((enrollment) => enrollment.user);
+ }
+ export function enrollUserInCourse(user, course) {
+  return model.create({ user, course });
+ }
+ export function unenrollUserFromCourse(user, course) {
+  return model.deleteOne({ user, course });
+ }
+ 
