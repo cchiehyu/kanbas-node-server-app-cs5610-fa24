5ae6a94190bd7b3300cafcb5668ee769019b40fd
Chieh-Yu <ccychen915@gmail.com>
2024-12-06 18:35:31 -0500

    Update QuizQuestionDao.js

--- Kanbas/Courses/Quizzes/QuizQuestions/QuizQuestionDao.js
+++ Kanbas/Courses/Quizzes/QuizQuestions/QuizQuestionDao.js
@@ -7,15 +7,8 @@ export const createQuestion = (question) => {
 export const findQuestionsForQuiz = async (quizId) => {
   console.log("DAO: Searching for questions with quizId:", quizId);
   try {
-    // First, let's see ALL questions in the database
     const allQuestions = await QuestionModel.find({});
-    console.log("DAO: ALL Questions in database:", JSON.stringify(allQuestions, null, 2));
-    console.log("DAO: Total questions in database:", allQuestions.length);
-
-    // Now let's see what we get with the filter
     const questions = await QuestionModel.find({ quizId }).sort({ order: 1 });
-    console.log("DAO: Filtered questions count:", questions.length);
-    console.log("DAO: Filtered Questions:", JSON.stringify(questions, null, 2));
     
     return questions;
   } catch (error) {
