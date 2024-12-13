ff6fab56238cd34ef17a51b566d75ff5e759c540
Chieh-Yu <ccychen915@gmail.com>
2024-12-12 19:34:09 -0500

    update the address

--- Kanbas/Courses/client.js
+++ Kanbas/Courses/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosWithCredentials = axios.create({ withCredentials: true });
 const REMOTE_SERVER = process.env.NETLIFY_URL;
-const COURSES_API = `${REMOTE_SERVER}/api/courses`;
+const COURSES_API = `https://kanbas-web-cs5610fa24-final-chiehyu.netlify.app/api/courses`;
 
 export const fetchAllCourses = async () => {
  const { data } = await axiosWithCredentials.get(COURSES_API);
--- Kanbas/Modules/client.js
+++ Kanbas/Modules/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const axiosWithCredentials = axios.create({ withCredentials: true });
 const REMOTE_SERVER = process.env.NETLIFY_URL;
-const MODULES_API = `${REMOTE_SERVER}/api/modules`;
+const MODULES_API = `https://kanbas-web-cs5610fa24-final-chiehyu.netlify.app/api/modules`;
 export const deleteModule = async (moduleId) => {
  const response = await axiosWithCredentials.delete(
    `${MODULES_API}/${moduleId}`
